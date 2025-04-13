2a8a5a613d673c9f7b8cac6e188d4805b089e989
BrandenXia <xxtbranden@outlook.com>
2025-04-12 22:47:41 -0700

    the search update

--- src/commands/rule.ts
+++ src/commands/rule.ts
@@ -1,5 +1,6 @@
 import { Rules } from "../db.ts";
 import type { CmdHandler } from "../commands.ts";
+import { Op } from "sequelize";
 
 const buildRuleEntry = async (rule: Rules) => {
   const id = (await rule.get("id")) as number;
@@ -8,33 +9,51 @@ const buildRuleEntry = async (rule: Rules) => {
   return `${id}. \`${regex}\` - \`${reaction}\``;
 };
 
+const buildRulesList = async (rules: Rules[]) =>
+  rules.length > 0
+    ? (await Promise.all(rules.map((entry) => buildRuleEntry(entry)))).join(
+        "\n",
+      )
+    : "No rules yet!";
+
 const handleRule: CmdHandler = async (msg, args) => {
   switch (args[0]) {
     case "add":
       await Rules.create({ regex: args[1], reaction: args[2] });
       await msg.reply("Rule added!");
       break;
-    case "list":
+    case "list": {
       let page = args.length > 1 ? parseInt(args[1]) : 1;
       const rules = await Rules.findAll({
         order: [["id", "ASC"]],
         limit: 10,
         offset: (page - 1) * 10,
       });
-      const rulesStr =
-        rules.length > 0
-          ? (
-              await Promise.all(rules.map((entry) => buildRuleEntry(entry)))
-            ).join("\n")
-          : "No rules yet!";
+      const rulesStr = await buildRulesList(rules);
       await msg.reply(rulesStr);
       break;
+    }
     case "delete":
       await Rules.destroy({
         where: { id: parseInt(args[1]) },
       });
       await msg.reply("Rule deleted!");
       break;
+    case "search": {
+      const similar = { [Op.iLike]: `%${args[1]}%` };
+      let page = args.length > 1 ? parseInt(args[2]) : 1;
+      const rules = await Rules.findAll({
+        where: {
+          [Op.or]: [{ regex: similar }, { reaction: similar }],
+        },
+        order: [["id", "ASC"]],
+        limit: 10,
+        offset: (page - 1) * 10,
+      });
+      const rulesStr = await buildRulesList(rules);
+      await msg.reply(rulesStr);
+      break;
+    }
   }
 };
 
